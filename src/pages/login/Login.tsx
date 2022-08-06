4156fe1e054f126e257b2745962db95c076fa86f
Vinicius <vinicezar800@hotmail.com>
2022-08-06 20:33:04 -0300

    feat: created login and registration form and logic

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,78 +1,230 @@
 import { Form, Formik } from "formik";
+import { useCallback, useState } from "react";
 import { Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import * as Yup from 'yup';
 
 import "./styles.scss";
 
 const Login = () => {
-
-  const handleLogin = (data: any) => {
-    console.log(data)
-  }
-
-  // Criando a interface
-  interface FormValuesType {
+  interface TypeLoginValues {
     email: string,
     password: string,
     stayConnected: boolean
   }
 
-  const formInitialValues: FormValuesType = {
+  interface TypeRegisterValues {
+    email: string,
+    password: string,
+    confirmPassword: string
+  }
+
+
+
+  const [formIsFlipped, setFormIsFlipped] = useState(false);
+
+  const handleLogin = useCallback(async (data: TypeLoginValues) => {
+    try {
+      const schema = Yup.object().shape({
+        password: Yup.string().min(8, 'Senha curta, por favor insira pelo menos 8 caracteres').required('Informe seu senha!'),
+        email: Yup.string().email('Email inválido!').required('Informe seu email!'),
+      });
+      await schema.validate(data);
+      console.log('Requisição sendo enviada para API...')
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        toast.error(`${error.message}`, {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    }
+  }, [])
+
+  const handleRegister = useCallback(async (data: TypeRegisterValues) => {
+    try {
+      const schema = Yup.object().shape({
+        confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'As senhas devem ser iguais!'),
+        password: Yup.string().min(8, 'Senha curta, por favor insira pelo menos 8 caracteres').required('Informe seu senha!'),
+        email: Yup.string().email('Email inválido!').required('Informe seu email!'),
+      });
+      await schema.validate(data);
+      console.log('Requisição sendo enviada para API...')
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        toast.error(`${error.message}`, {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      }
+    }
+  }, [])
+
+  const loginInitialValues: TypeLoginValues = {
     email: '',
     password: '',
     stayConnected: false,
   }
 
+  const registerInitialValue: TypeRegisterValues = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+  }
+
   return (
     <>
       <section className="login">
-        <Formik
-          enableReinitialize
-          onSubmit={handleLogin}
-          initialValues={formInitialValues}
-        >
-          {({ values, handleBlur, handleChange, handleSubmit }) => (
-            <Form className="form-horizontal">
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  onChange={handleChange("email")}
-                />
-                <label className={`form-label-anim ${values.email ? 'focused' : 'unfocused'}`} htmlFor="">Email</label>
-              </div>
-              <div className="form-group">
-                <input
-                  type="password"
-                  className="form-control"
-                  onChange={handleChange("password")}
-                />
-                <label className={`form-label-anim ${values.password ? 'focused' : 'unfocused'}`} htmlFor="">Senha</label>
-              </div>
-              <div className="form-group">
-                <div className="form-stay-connected">
-                  <input type="checkbox" name="stayconnected" id="stayconnected" />
-                  <label htmlFor="stayconnected">Mantenha-me conectado</label>
+        <ToastContainer />
+        <div className={`flip-form ${formIsFlipped ? 'active' : ''}`}>
+          <Formik
+            enableReinitialize
+            onSubmit={handleLogin}
+            initialValues={loginInitialValues}
+          >
+            {({ values, handleBlur, handleChange, handleSubmit }) => (
+              <Form className="form-horizontal form-front">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    id="email"
+                    className="form-control"
+                    autoComplete="email"
+                    onChange={handleChange("email")}
+                  />
+                  <label
+                    className={`form-label-anim ${values.email ? "focused" : "unfocused"}`}
+                    htmlFor="email"
+                  >
+                    Email
+                  </label>
                 </div>
-
-                <button type="submit" className="btn-submit">Entrar</button>
-              </div>
-              <div className="form-group">
-                <hr className="form-divisor" />
-
-                <div className="form-flex-wrap">
-                  <div className="form-action">
-                    <Link className="form-action-link" to="/">Recuperar senha</Link>
-                  </div>
-                  <hr className="form-divisor-circle" />
-                  <div className="form-action">
-                    <Link className="form-action-link" to="/register">Criar nova conta</Link>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    autoComplete="current-password"
+                    onChange={handleChange("password")}
+                  />
+                  <label
+                    className={`form-label-anim ${values.password ? "focused" : "unfocused"}`}
+                    htmlFor="password"
+                  >
+                    Senha
+                  </label>
+                </div>
+                <div className="form-group">
+                  <div className="form-stay-connected">
+                    <input
+                      type="checkbox"
+                      name="stayconnected"
+                      id="stayconnected"
+                    />
+                    <label htmlFor="stayconnected">Mantenha-me conectado</label>
                   </div>
                 </div>
-              </div>
-            </Form>
-          )}
+                <div className="form-group">
+                  <button type="submit" className="btn-submit">
+                    Entrar
+                  </button>
+                </div>
+                <div className="form-group">
+                  <hr className="form-divisor" />
 
-        </Formik>
+                  <div className="flex justify-content-center align-items-center">
+                    <Link className="form-action-link" to="/">
+                      Recuperar senha
+                    </Link>
+                    <hr className="form-divisor-circle" />
+                    <button type="button" className="form-action-link" onClick={() => setFormIsFlipped(!formIsFlipped)}>
+                      Criar nova conta
+                    </button>
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
+
+          <Formik
+            enableReinitialize
+            onSubmit={handleRegister}
+            initialValues={registerInitialValue}
+          >
+            {({ values, handleBlur, handleChange, handleSubmit }) => (
+              <Form className="form-horizontal form-back">
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    autoComplete="email"
+                    onChange={handleChange("email")}
+                  />
+                  <label
+                    className={`form-label-anim ${values.email ? "focused" : "unfocused"}`}
+                    htmlFor=""
+                  >
+                    Email
+                  </label>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    id="passwordRegister"
+                    className="form-control"
+                    autoComplete="current-password"
+                    onChange={handleChange("password")}
+                  />
+                  <label
+                    className={`form-label-anim ${values.password ? "focused" : "unfocused"}`}
+                    htmlFor="passwordRegister"
+                  >
+                    Senha
+                  </label>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    id="confirmPassword"
+                    className="form-control"
+                    autoComplete="current-password"
+                    onChange={handleChange("confirmPassword")}
+                  />
+                  <label
+                    className={`form-label-anim ${values.confirmPassword ? "focused" : "unfocused"}`}
+                    htmlFor="confirmPassword"
+                  >
+                    Confirme sua senha
+                  </label>
+                </div>
+                <div className="form-group">
+                  <button type="submit" className="btn-submit">
+                    Cadastrar
+                  </button>
+                </div>
+                <div className="form-group">
+                  <hr className="form-divisor" />
+
+                  <div className="flex justify-content-center">
+                    <button type="button" className="form-action-link" onClick={() => setFormIsFlipped(!formIsFlipped)}>
+                      Já possui uma conta?
+                    </button>
+                  </div>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
       </section>
     </>
   );
