b6bec88a2d697c1ea272c9dffcfbd6fb485d2464
Vinicius <vinicezar800@hotmail.com>
2022-08-22 21:37:58 -0300

    feat: button clear board data

--- src/pages/test/Test.tsx
+++ src/pages/test/Test.tsx
@@ -1,12 +1,23 @@
-import { useContext, useEffect } from "react";
-import { BoardContext } from "../../hooks/context/BoardContext/BoardContext";
 import Board from "./components/Board";
+import { useNavigate } from "react-router-dom";
 
 import "./styles.scss";
 
 const Test = () => {
+  let navigate = useNavigate();
+
   return (
     <>
+      <div className="flex wrap-remove-storage">
+        <button
+          onClick={() => {
+            localStorage.clear();
+            navigate(0);
+          }}
+        >
+          Limpar
+        </button>
+      </div>
       <section className="test-section">
         <Board />
       </section>
