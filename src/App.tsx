671f4a2af75961012e31f989eff2d671d5bac93e
Vinicius <vinicezar800@hotmail.com>
2022-09-26 20:29:43 -0300

    chore: remove test routes and imports/exports

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar/Navbar";
 import PrivateRoute from "./components/Private/PrivateRoute";
 import AuthContextProvider from "./hooks/context/AuthContext";
-import BoardContextProvider from "./hooks/context/BoardContext";
-import { Home, Workspace, SignIn, SignUp, Board, Test } from "./pages";
+import { Home, Workspace, SignIn, SignUp, Board } from "./pages";
 import GlobalStyles from "./assets/styles/Global";
 
 const App = () => {
@@ -18,30 +17,27 @@ const App = () => {
       <AuthContextProvider>
         <BrowserRouter>
           {/* <Navbar /> */}
-          <BoardContextProvider>
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path="/login" element={<SignIn />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route
-                path="/workspace"
-                element={
-                  <PrivateRoute>
-                    <Workspace />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/board/:boardId"
-                element={
-                  <PrivateRoute>
-                    <Board />
-                  </PrivateRoute>
-                }
-              />
-              <Route path="/testes" element={<Test />} />
-            </Routes>
-          </BoardContextProvider>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route
+              path="/workspace"
+              element={
+                <PrivateRoute>
+                  <Workspace />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/board/:boardId"
+              element={
+                <PrivateRoute>
+                  <Board />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
         </BrowserRouter>
       </AuthContextProvider>
     </>
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,6 +3,5 @@ import Workspace from "./Workspace";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import Board from "./Board";
-import Test from "./test/Test";
 
-export { Home, Workspace, SignIn, SignUp, Board, Test };
+export { Home, Workspace, SignIn, SignUp, Board };
