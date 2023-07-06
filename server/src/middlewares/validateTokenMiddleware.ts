07018f54c0be8e9b7d386e14d35ebc513014b85b
Vinicius <vinicezar800@hotmail.com>
2023-07-06 19:46:07 -0300

    style: format code according to ESLint pattern

--- server/src/middlewares/validateTokenMiddleware.ts
+++ server/src/middlewares/validateTokenMiddleware.ts
@@ -1,23 +1,28 @@
-import { Request, Response, NextFunction } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
 
-export default function validateTokenMiddleware(req: Request, res: Response, next: NextFunction) {
-  const { authorization } = req.headers;
-  const token = authorization?.replace("Bearer ", "");
+export default function validateTokenMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { authorization } = req.headers
+  const token = authorization?.replace('Bearer ', '')
 
   if (!token) {
-    return res.sendStatus(401);
+    return res.sendStatus(401)
   }
 
   try {
     const decoded = jwt.verify(
       token,
-      process.env.JWT_SECRET as string
-    ) as JwtPayload;
-    res.locals.userId = decoded.id;
+      process.env.JWT_SECRET as string,
+    ) as JwtPayload
 
-    next();
+    res.locals.userId = decoded.id
+
+    next()
   } catch (error) {
-    res.sendStatus(401);
+    res.sendStatus(401)
   }
 }
